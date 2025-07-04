8fa0b7547dbb29b1af36d678dd7a49a4adb262e6
Srimoun <gottamsrimouni@karunya.edu.in>
2025-07-04 19:08:15 +0530

    completed imdb application

--- src/Components/Watchlist.jsx
+++ src/Components/Watchlist.jsx
@@ -4,7 +4,32 @@ import genreids from '../assets/genre';
 
   const Watchlist = () => {
    const [watchlist, setWatchlist] = useState([]);
+   const[search, setSearch] = useState("");
+   const [genreList, setGenreList] = useState([]);
+   const[currGenre, setCurrGenre] = useState("All");
 
+   const handleSearch = (e) => {
+    setSearch(e.target.value);
+   }
+
+   const handleFilter = (genre) => {
+    setCurrGenre(genre);
+   };
+
+  const handleascendingratings = () =>{
+    let sortedAscending = watchlist.sort((a, b) => {
+      return a.vote_average - b.vote_average;
+    });
+    setWatchlist([...sortedAscending]);
+
+  }
+
+  const handledescedingratings = () =>{
+    let sorteddescending = watchlist.sort((a,b) => {
+      return b.vote_average - a.vote_average;
+    });
+      setWatchlist([...sorteddescending]);
+  }
    
    useEffect(() => {
       let moviesfromlocalstorage = localStorage.getItem("watchList");
@@ -13,20 +38,69 @@ import genreids from '../assets/genre';
           return
        }
 
-       setWatchlist(JSON.parse(moviesfromlocalstorage));
-       console.log(watchlist);
+       const parsedWatchList = JSON.parse(moviesfromlocalstorage);
+
+       setWatchlist(parsedWatchList);
+       console.log(parsedWatchList);
       
-   }, []);
+      }, []);
+
+   
+   // let genres = parsedWatchList.map((movie) => {
+  //    return genreids[movie.genre_ids[0]]
+  //  })
+
+  //  genres = new Set(genres);
+  //  setGenreList([...genres]);
+    
+  // }, []);
+  useEffect(() => {
+    console.log(watchlist);
+    let genres = watchlist.map((movie) => {
+      return genreids[movie.genre_ids[0]];
+    
+  })
+
+  genres = new Set(genres);
+  setGenreList(["All", ...genres]);
+  console.log(genres);
+}, [watchlist])
 
   return (
     <div className="overflow-hidden rounded-lg border border-gray-200 shadow-md m-5">
+      {/* Search filed */}
+      <div className = "flex justify-center my-10">
+        <input type = "text" placeholder = "Search movies" value = {search} onChange={handleSearch} className='border border-gray-500 h-[3rem] 
+         w-[18rem] px-3' ></input>
+      </div>
+       {/* Genre */}
+       <div className="flex justify-center flex-wrap gap-4 my-4">
+        {genreList && genreList.map((genre) => {
+          return (
+            <div key = {genre} onClick={() => handleFilter(genre)} className ={currGenre == genre ? "mx-4 flex justify-center items-center bg-blue-400 h-[3rem] w-[9rem] text-white font-bold border rounded-xl cursor-pointer": 
+              "mx-4 flex justify-center items-center bg-gray-400 h-[3rem] w-[9rem] text-white border rounded-xl cursor-pointer" }
+            >
+              {genre}
+            </div>
+          )
+        })}
+       </div>
+
       <table className="w-full border-collapse bg-white text-left text-sm text-gray-500">
         <thead>
           <tr className="bg-gray-50">
             <th className="px-6 py-4 font-medium text-gray-900">Name</th>
             <th>
-              <div className="flex">
+              <div className="flex items-center gap-2">
+                <i
+              onClick={handleascendingratings}
+              className="fa-solid fa-arrow-up"
+              ></i>
                 <div>Ratings</div>
+               <i
+              onClick={handledescedingratings}
+              className="fa-solid fa-arrow-down"
+              ></i>
               </div>
             </th>
             <th>
@@ -46,7 +120,18 @@ import genreids from '../assets/genre';
           
           {/* Sample Movie Element */}
 
-          {watchlist.map((movieObj)  => (
+          {watchlist
+          .filter((movie) => {
+              if(currGenre === "All"){
+                return true;
+              }
+
+              return genreids[movie.genre_ids[0]] === currGenre;
+          })
+          .filter((movie) =>
+            movie.title.toLowerCase().includes(search.toLowerCase())
+          )
+          .map((movieObj)  => (
               <tr key = {movieObj.id} className="hover:bg-gray-50">
             <td className="flex items-center px-6 py-4 font-normal text-gray-900">
               <img
@@ -68,8 +153,8 @@ import genreids from '../assets/genre';
       </table>
     </div>
   );
-};
 
+  }
 
 
 export default Watchlist;
